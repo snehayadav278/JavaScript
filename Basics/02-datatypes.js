5cbb08e6bc491ac1f436ef2a6b081c167c049780
snehayadav278 <snehay851@gmail.com>
2026-05-28 14:48:19 +0530

    learned dataypes

---
+++ Basics/02-datatypes.js
@@ -0,0 +1,21 @@
+"use strict" ; //treat all JS code as newer version
+
+// alert (3+3 )  //we are using nodejs , not browser
+
+let name = "Sneha"
+let age = 18
+let isLoggedIn = false
+let state ;
+
+// number (Integer + decimal dono cover karta hai)
+// bigint (Very large numbers ke liye)
+// string => " "
+// boolean => true / false
+// null  (Intentional empty value)
+// undefined (Variable declare hua but value assign nahi hui)
+// symbol (Unique identifier (advanced use))
+
+
+//object(Key-value pair)
+console.log(typeof undefined);
+console.log(typeof null);
