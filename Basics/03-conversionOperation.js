58266675afa16bad2233c0771e1712383a836df5
snehayadav278 <snehay851@gmail.com>
2026-05-29 21:43:04 +0530

    conversion Operation

--- Basics/03-conversionOperation.js
+++ Basics/03-conversionOperation.js
@@ -26,3 +26,29 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+
+
+//************ Operations  ************/
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = "sneha"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100 
+gameCounter++;
+console.log(gameCounter);
