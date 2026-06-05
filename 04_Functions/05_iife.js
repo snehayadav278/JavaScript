cc677f4db77b9ae8a665e1c9e930dc00a8550c88
snehayadav278 <snehay851@gmail.com>
2026-06-05 12:28:33 +0530

    iife

---
+++ 04_Functions/05_iife.js
@@ -0,0 +1,15 @@
+//Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+//  ()()
+
+( (name) => {
+    console.log(`DB CONNECTED 2 ${name}`)
+}) ('sneha');
+
+
+
