26d6f4f1715d7a2ea6a98a90a2a938b873826def
snehayadav278 <snehay851@gmail.com>
2026-06-06 17:01:45 +0530

    terniary operator

--- 05_ControlFlow/03_truthy.js
+++ 05_ControlFlow/03_truthy.js
@@ -44,3 +44,27 @@ const emptyObject = {}
 if(Object.keys(emptyObject).length === 0){
     console.log("object is empty");
 }
+
+
+// false == 0 (true)
+// false == '' (true)
+// 0 == '' (true)
+
+
+
+//Nullish Coalescing Operator (??) : null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1);
+
+
+//Terniary Operator
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
