2c2402411794ec9524b13957fed584981ad218e4
snehayadav278 <snehay851@gmail.com>
2026-06-07 22:52:34 +0530

    forEach

--- 06_Iterations/05_forEach.js
+++ 06_Iterations/05_forEach.js
@@ -1,22 +1,43 @@
 const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
 
-coding.forEach( function (item){   //callback function(no name)
+// coding.forEach( function (item){   //callback function(no name)
+//     console.log(item);
+// } )
+
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe);
+
+
+// coding.forEach( (item , index , arr) => {
+//     console.log(item , index , arr);
+// } )
+
+
+const myCoding = [
+    {
+        languageName : "javascript" ,
+        languageFileName : "js"
+    } ,
+    {
+        languageName : "java" ,
+        languageFileName : "java"
+    } ,
+    {
+        languageName : "python" ,
+        languageFileName : "py"
+    }
+]
+
+myCoding.forEach( (item) => {
     console.log(item);
-} )
-
-
-coding.forEach( (item) => {
-    console.log(item);
-} )
-
-
-function printMe(item){
-    console.log(item);
-}
-
-coding.forEach(printMe);
-
-
-coding.forEach( (item , index , arr) => {
-    console.log(item , index , arr);
+    console.log(item.languageName);
 } )
