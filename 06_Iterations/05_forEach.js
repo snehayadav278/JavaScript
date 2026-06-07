fcbe01bada96d47c4278a0f597fb65958f4aa605
snehayadav278 <snehay851@gmail.com>
2026-06-07 22:48:01 +0530

    for Each

---
+++ 06_Iterations/05_forEach.js
@@ -0,0 +1,22 @@
+const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
+
+coding.forEach( function (item){   //callback function(no name)
+    console.log(item);
+} )
+
+
+coding.forEach( (item) => {
+    console.log(item);
+} )
+
+
+function printMe(item){
+    console.log(item);
+}
+
+coding.forEach(printMe);
+
+
+coding.forEach( (item , index , arr) => {
+    console.log(item , index , arr);
+} )
