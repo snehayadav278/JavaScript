e115f43444b151fe39235fa5ba5d434cc4a97683
snehayadav278 <snehay851@gmail.com>
2026-06-08 16:10:02 +0530

    DOM Selectors

---
+++ 07_DOM/03_selectors.js
@@ -0,0 +1,19 @@
+// ID selector
+const heading = document.getElementById("main-heading");
+console.log(heading);
+
+// Class selector
+const paragraphs = document.getElementsByClassName("text");
+console.log(paragraphs);
+
+// Tag selector
+const divs = document.getElementsByTagName("div");
+console.log(divs);
+
+// Query selector
+const firstPara = document.querySelector(".text");
+console.log(firstPara);
+
+// Query selector all
+const allPara = document.querySelectorAll(".text");
+console.log(allPara);
