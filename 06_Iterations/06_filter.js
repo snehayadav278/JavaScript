4db2bfecaa777e885168d8494d575ad17b6ccc76
snehayadav278 <snehay851@gmail.com>
2026-06-08 15:41:23 +0530

    filter function

--- 06_Iterations/06_filter.js
+++ 06_Iterations/06_filter.js
@@ -1,11 +1,11 @@
-//const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
+const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
 
-// const values = coding.forEach( (item) => {
-//     console.log(item);
-//     return item;
-// })
+const values = coding.forEach( (item) => {
+    console.log(item);
+    return item;
+})
 
-// console.log(values);
+console.log(values);
 
 
 const myNums = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -52,4 +52,4 @@ let userBooks = books.filter( (bk) => bk.genre == 'History' )
 userBooks = books.filter( (bk) => {
     return bk.publish >= 2000
 })
-console.log(userBooks);
+console.log(userBooks); 
