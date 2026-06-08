4c1b8fb825caf4e6201385f52723557b57d1e86a
snehayadav278 <snehay851@gmail.com>
2026-06-08 15:41:54 +0530

    map function

--- 06_Iterations/07_map.js
+++ 06_Iterations/07_map.js
@@ -3,6 +3,8 @@ const myNums = [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const newNums = myNums.map((num) => num + 10)
 console.log(newNums);
 
+
+//chaining
 const newNums2 = myNums
                 .map((num) => num*10 )
                 .map((num) => num + 1 )
