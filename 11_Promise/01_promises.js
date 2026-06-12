8082de692e3b59db1ab405079fa6676722eb3e57
snehayadav278 <snehay851@gmail.com>
2026-06-12 22:07:45 +0530

    Added GitHub API fetch example

--- 11_Promise/01_promises.js
+++ 11_Promise/01_promises.js
@@ -106,3 +106,14 @@ fetch('https://jsonplaceholder.typicode.com/users')
     console.log(data);
 })
 .catch((error) => console.log(error))
+
+
+
+fetch('https://api.github.com/users/snehayadav278')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error));
